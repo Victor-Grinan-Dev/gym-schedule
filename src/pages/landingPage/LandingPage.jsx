3391f0acc6997c164ccec021ffe7894e5cd95080
Victor <victorgrinan@gmail.com>
2025-03-29 19:37:12 +0200

    fix error in day columns

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -8,13 +8,13 @@ import Today from "../../components/today/Today";
 import getSheetData from "../../service/getSheetData";
 
 const weekdays = {
-  0: "A",
-  1: "B",
-  2: "C",
-  3: "D",
-  4: "E",
-  5: "F",
-  6: "G",
+  0: "B",
+  1: "C",
+  2: "D",
+  3: "E",
+  4: "F",
+  5: "G",
+  6: "F",
 };
 
 const LandingPage = () => {
