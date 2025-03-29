fff800a93377b21ecb64e882990e04a392924566
Victor <victorgrinan@gmail.com>
2025-03-29 19:35:45 +0200

    works but needs improvements

--- src/components/today/Today.jsx
+++ src/components/today/Today.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
-const Today = (data) => {
-    // const {weekday, stateData} = data;
-    console.log(data)
+const Today = (props) => {
+    const {stateData} = props;
   return (
     <section className="Today" style={{width:"100%", color:"white"}}>
     <h2>Today</h2>
-    {/* {
+    {
       stateData && stateData.map((d, i)=>(
-        <p key={i} >{d[weekday]}</p>
+        <p key={i} >{d}</p>
       ))
-    } */}
+    }
   </section>
   )
 }
 
 export default Today;
+
+export async function getServerSideProps() {
+  return
+}
--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -2,37 +2,43 @@ import React, { useEffect, useState } from "react";
 // import { HashLink } from "react-router-hash-link";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+
 /* COMPONENTS */
 import Today from "../../components/today/Today";
+import getSheetData from "../../service/getSheetData";
+
+const weekdays = {
+  0: "A",
+  1: "B",
+  2: "C",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "G",
+};
 
 const LandingPage = () => {
-  const [stateData, setData] = useState();
-
-  // const endpoint = import.meta.env.VITE_APP_ENDPOINT
-  const endpoint = "https://script.google.com/macros/s/AKfycbxNWgf2NnoqlrSE7FlC9neUd8Jxj02tgcF2v_hDE1ZuJEbhITWgk9XWR4oHIxaraNyW/exec"
-  console.log(endpoint);
-
-  const getData = async () => {
-    try {
-        const res = await fetch(endpoint);
-        const data = await res.json();
-        setData(data);
-        
-    }catch (error) {
-      console.log(`${JSON.stringify(error)}`);
-    }
-  }
-
-  useEffect(() => {
-      getData();
-      // eslint-disable-next-line
-  }, [])
-
+  const [data, setData] = useState([]);
   const today = new Date().getDay();
+  const query = `SELECT ${weekdays[today]}`;
+  const sheetID = "1qIwYwmFuzTNTtM_qL5yytH2ZwIDOg4BvXxuAUyP97JM";
+
+  useEffect(() => {   
+    getSheetData({
+      sheetID: sheetID,
+      sheetName: 'Sheet1',
+      query: query,
+      callback: (data) => {
+        setData(data);
+      },
+    })
+  }
+  , [])
+
  
   return (
     <div className='landinpage' id="landingpage">
-        <Today weekday={today} stateData={stateData} />
+        <Today weekday={today} stateData={data} />
     </div>
   )
 }
